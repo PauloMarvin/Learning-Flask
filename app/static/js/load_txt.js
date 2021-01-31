331cc3907d5bf0a306270760e457bb41b02d8f47
PauloMarvin <paulo.marving@gmail.com>
2021-01-31 00:26:42 -0300

    Loading txt file, exibing and cleaning contend on the page

---
+++ app/static/js/load_txt.js
@@ -0,0 +1,37 @@
+let input = document.querySelector('#input')
+let textbox = document.querySelector('#caixa_texto')
+let btnclear = document.querySelector('#clear')
+
+
+
+input.addEventListener('change', () => {
+    let files = input.files;
+
+    if (files.length === 0) return;
+
+    const file = files[0];
+
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+        const file = e.target.result;
+        console.log(file);
+
+        const lines = file.split(/\r\n|\n/);
+        textbox.value = lines.join('\n');
+        console.log(lines);
+
+    };
+
+    reader.onerror = (e) => alert(e.target.error.name);
+
+    reader.readAsText(file);
+});
+
+
+
+btnclear.addEventListener('click', () => {
+    textbox.value = '';
+
+});
+
