20d92ce064a4d1c0d1fc5819ec39e9dac0667811
PauloMarvin <paulo.marving@gmail.com>
2021-02-06 23:39:12 -0300

    Basic authentication with JSON Web Tokens

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,28 @@
-from flask import render_template,request
+from flask import render_template,request,jsonify,make_response,Flask
+import jwt
+import datetime
 from app import app
+from functools import  wraps
 from ..models import usuario_teste
+
+def token_validation(f):
+    @wraps(f)
+    def decorated(*args,**kwargs):
+        token = request.args.get('token')
+
+        if not token:
+            return ({'mensages': 'Sem token'}),403
+
+        try:
+            data = jwt.decode(token,app.config['SECRET_KEY'])
+        except:
+            return jsonify({'mensages': 'token nao correspondente ' }), 403
+
+        return f(*args, **kwargs)
+
+    return decorated
+
+
 
 
 @app.route('/')
@@ -35,6 +57,7 @@ def mensages():
 
 
 @app.route('/atividade2-load')
+@token_validation
 def load():
     return render_template('requisitionsL.html')
 
@@ -44,11 +67,25 @@ def conteudo():
     return render_template('content.html')
 
 @app.route('/pagina-login')
-def pagina_login():
-    return render_template('login-page.html')
+def login():
+    auth = request.authorization
+
+    if auth and auth.password == 'password' and auth.username =='paulo':
+        token = jwt.encode({'user' : auth.username, 'exp':datetime.datetime.utcnow()+datetime.timedelta(seconds=5)},app.config['SECRET_KEY'])
+
+        return jsonify({'token': token.decode('UTF-8')})
+
+    return make_response('teste',401,{'WWW-Authenticate':'Basic realm="Login Required"'})
+
+
+
+
+
 
 @app.route('/pagina-protegida')
+@token_validation
 def pagina_protegida():
     return render_template('pagina-protegida.html')
+
 
 
--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from app import manager
 from app import app
 
 if __name__ == "__main__":
-    manager.run()
+    app.run()
 
